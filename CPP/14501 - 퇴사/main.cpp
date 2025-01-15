ed8ea963d8c39631eacfe0eab5f6b15460f33fee
Jin Yoon <jinjjij0226@gmail.com>
2025-01-15 15:55:18 +0900

    0115 정리

---
+++ CPP/14501 - 퇴사/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int N = 0;
+    std::cin >> N;
+
+    
+    
+    return 0;
+}
