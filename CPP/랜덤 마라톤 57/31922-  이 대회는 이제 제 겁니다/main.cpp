7ab07e2ced1ce664998b25fe483c4dddc5abda19
Jin Yoon <jinjjij0226@gmail.com>
2025-07-03 22:03:48 +0900

    0703 - 일퀘

--- CPP/랜덤 마라톤 57/31922-  이 대회는 이제 제 겁니다/main.cpp
+++ CPP/랜덤 마라톤 57/31922-  이 대회는 이제 제 겁니다/main.cpp
@@ -6,7 +6,10 @@ int main(){
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::cout.tie(NULL);
-
+    
+    int a,p,c;
+    std::cin>>a>>p>>c;
+    std::cout << std::max(a+c,p);
     
     return 0;
 }
